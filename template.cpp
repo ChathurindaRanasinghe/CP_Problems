1bb0fa6e09bc74a41437cc49b7847560d0d91d90
Chathurinda <chathurindaranasinghe@gmail.com>
2022-02-26 13:10:23 +0530

    added new template

--- template.cpp
+++ template.cpp
@@ -5,36 +5,46 @@ using namespace std;
 
 typedef long long ll;
 
-#define deb(x) cout<<#x << " = " << x << "\n";
-#define vi vector<int>
-#define eb emplace_back
-#define pb push_back
-#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define deb(x) cout << #x << " = " << x << "\n";
 
-#define re_srt(a) sort(a.rbegin(),a.rend())
-#define srt(a) sort(a.begin(),a.end())
-#define all(a) a.begin(),a.end()
+#define re_srt(a) sort(a.rbegin(), a.rend())
+#define srt(a) sort(a.begin(), a.end())
+#define all(a) a.begin(), a.end()
 
-#define read(a,n){for(int i=0;i< n;i++){ cin >> a[i];}}
+#define read(a, n)                  \
+    {                               \
+        for (int i = 0; i < n; i++) \
+        {                           \
+            cin >> a[i];            \
+        }                           \
+    }
 
-//for unordered map
-struct hash_pair{template <class T1,class T2>size_t operator()(const pair<T1,T2>& p) const{auto hash1 = hash<T1>{}(p.first);auto hash2=hash<T2>{}(p.second);return hash1^hash2;}};
+// for unordered map
+struct hash_pair
+{
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2> &p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2;
+    }
+};
 
-string s; ll n;
+string s;
+ll n;
 
-void solve(){
-    
+void solve()
+{
 }
 
-
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
     int t = 1;
-    //cin >> t;
-    while(t--)
+    // cin >> t;
+    while (t--)
         solve();
 }
-
-
